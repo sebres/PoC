fd9c655b7394addc0071cb4e61a1e20e78358639
sebres <serg.brester@sebres.de>
2018-09-05 20:01:46 +0200

    test-dump-details.cmd: more details as output into file "test-result.txt"

--- SB-0D-001-win-exec/test-dump-details.cmd
+++ SB-0D-001-win-exec/test-dump-details.cmd
@@ -1,10 +1,32 @@
 @pushd %~dp0
 @rem echo %*
+@echo CMDCMDLINE: > test-result.txt
+@echo ^  %CMDCMDLINE% >> test-result.txt
+@echo JOINED (%%*): >> test-result.txt
+@echo ^  %* >> test-result.txt
+@echo SINGLY (%%1..%%9): >> test-result.txt
+@echo ^  1:%1 >> test-result.txt
+@echo ^  2:%2 >> test-result.txt
+@echo ^  3:%3 >> test-result.txt
+@echo ^  4:%4 >> test-result.txt
+@echo ^  5:%5 >> test-result.txt
+@echo ^  6:%6 >> test-result.txt
+@echo ^  7:%7 >> test-result.txt
+@echo ^  8:%8 >> test-result.txt
+@echo ^  9:%9 >> test-result.txt
+@echo EN-QUOTED ("%%~1".."%%~9"): >> test-result.txt
+@echo ^  1:"%~1" >> test-result.txt
+@echo ^  2:"%~2" >> test-result.txt
+@echo ^  3:"%~3" >> test-result.txt
+@echo ^  4:"%~4" >> test-result.txt
+@echo ^  5:"%~5" >> test-result.txt
+@echo ^  6:"%~6" >> test-result.txt
+@echo ^  7:"%~7" >> test-result.txt
+@echo ^  8:"%~8" >> test-result.txt
+@echo ^  9:"%~9" >> test-result.txt
 @test-dump.exe 9:%9 8:%8 7:%7 6:%6 5:%5 4:%4 3:%3 2:%2 1:%1 && echo ^ - revers ^(details^)
 @test-dump.exe %9 %8 %7 %6 %5 %4 %3 %2 %1 && echo ^ - revers
 @test-dump.exe 1:%1 2:%2 3:%3 4:%4 5:%5 6:%6 7:%7 8:%8 9:%9 && echo ^ - normal ^(details^)
 @test-dump.exe %1 %2 %3 %4 %5 %6 %7 %8 %9 && echo ^ - normal
-@rem test-dump.exe "9:%~9" "8:%~8" "7:%~7" "6:%~6" "5:%~5" "4:%~4" "3:%~3" "2:%~2" "1:%~1" && echo ^ - revers ^(unquoted^)
-@rem test-dump.exe "1:%~1" "2:%~2" "3:%~3" "4:%~4" "5:%~5" "6:%~6" "7:%~7" "8:%~8" "9:%~9" && echo ^ - normal ^(unquoted^)
 @test-dump.exe %*
 @popd
